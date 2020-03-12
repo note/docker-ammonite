7680b9c0f818605e6eba334eebf90fc405356ec6
Michał Sitko <pierwszy1@gmail.com>
2020-03-12 11:32:38 +0100

    Travis build fix

--- ci/push.sh
+++ ci/push.sh
@@ -5,7 +5,9 @@ set -e
 
 docker_build () {
 	local SCALA_VERSION="$1"
-	docker build --build-arg SCALA_VERSION=$SCALA_VERSION --build-arg AMMONITE_VERSION=$TRAVIS_TAG . -t ammonite:$SCALA_VERSION-$TRAVIS_TAG	
+	local FULL_TAG="$SCALA_VERSION-$TRAVIS_TAG"
+	docker build --build-arg SCALA_VERSION=$SCALA_VERSION --build-arg AMMONITE_VERSION=$TRAVIS_TAG . -t ammonite:$FULL_TAG
+	docker tag ammonite:$FULL_TAG note/ammonite:$FULL_TAG
 }
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
@@ -13,9 +15,6 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker_build "2.12"
 docker_build "2.13"
 
-docker tag ammonite:2.12 note/ammonite:2.12
-
-docker tag ammonite:2.13 note/ammonite:2.13
-docker tag ammonite:2.13 note/ammonite:latest
+docker tag ammonite:2.13-$TRAVIS_TAG note/ammonite:latest
 
 docker push note/ammonite
